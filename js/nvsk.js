c424682384dba553018e45d17d8cf921ac2003fb
alexandra94906 <32568785+alexandra94906@users.noreply.github.com>
2017-10-06 18:34:47 +0700

    web

---
+++ js/nvsk.js
@@ -0,0 +1,12 @@
+var mapdata = 
+{
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "label": "rr", "image": "http:\/\/sgugit.ru\/news\/img\/priem2015\/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA7.jpg" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 82.862612526418019, 54.987105718612376 ], [ 82.8629591189124, 54.987120727000253 ], [ 82.86309644801392, 54.986092639456963 ], [ 82.862612526418019, 54.987105718612376 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 1, "label": "rr", "image": "http:\/\/sgugit.ru\/news\/img\/priem2015\/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA7.jpg" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 82.862612526418019, 54.987101966514544 ], [ 82.862965658393378, 54.987120727000253 ], [ 82.86309644801392, 54.986092639456963 ], [ 82.862756395000503, 54.986070126296433 ], [ 82.862612526418019, 54.987101966514544 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 1, "label": "rr", "image": "http:\/\/sgugit.ru\/news\/img\/priem2015\/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA7.jpg" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 82.862619065898997, 54.987113222807025 ], [ 82.8629591189124, 54.987124479096344 ], [ 82.86309644801392, 54.986107648223651 ], [ 82.862769473962558, 54.986092639456963 ], [ 82.862619065898997, 54.987113222807025 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 1, "label": "ooo", "image": "http:\/\/sgugit.ru\/news\/img\/priem2015\/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA7.jpg" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 82.86359998805294, 54.98725955432203 ], [ 82.863730777673481, 54.986505378495742 ], [ 82.863475737913376, 54.986512882802629 ], [ 82.863344948292834, 54.98726330640514 ], [ 82.86359998805294, 54.98725955432203 ] ] ] } }
+]
+}
